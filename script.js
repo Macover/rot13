7558b1f918969b3e547747835df069d36b58c44e
Macover <macover1507@hotmail.com>
2022-05-29 18:37:33 -0500

    funciones para resolver de matriz a matriz  inversa y a rot13 de nuevo

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const messageInputDE = document.getElementById('messageInputDE');
 const rotInputDE = document.getElementById('rotInputDE');
 const matrixInputDE = document.getElementById('matrixInputDE');
 
+const rotInputEN = document.getElementById('rotInputEN');
+const inverseInputMatrixDE = document.getElementById('inverseInputMatrixDE');
+
 
 /* CONSTANTS */
 const LATIN_ALPHABET = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -22,15 +25,57 @@ const INVERSE_MATRIX = [
 messageInputDE.addEventListener('input',(e)=>{
 
     const ORIGINAL_MESSAGE = e.target.value;    
+    
     /* call the method to return the new string and insert it into the rotInputDE's value */
-    const encryptedRot13Message = decodeRot13(ORIGINAL_MESSAGE);
+    const encryptedRot13Message = encodeRot13(ORIGINAL_MESSAGE);
     rotInputDE.value = encryptedRot13Message;
 
     /* call the method to return the new string and insert it into the matrixInputDE's value */
-    const encryptedMatrixMessage = decodeMatrix(encryptedRot13Message);
+    const encryptedMatrixMessage = encodeMatrix(encryptedRot13Message);
     matrixInputDE.value = encryptedMatrixMessage;
 
+    /* call the method to return the new string and insert it into the inverseInputMatrixDE's value */
+    const decryptedMatrixMessage = decodeMatrix(encryptedMatrixMessage);
+    inverseInputMatrixDE.value = decryptedMatrixMessage; 
+
+    /* call the method to return the new string and insert it into the matrixInputEN's value */
+    const initialMessage = decodeMessageMatrix(decryptedMatrixMessage);
+    rotInputEN.value = initialMessage;
 });
+
+const decodeNumbers = array =>{
+    const newArray = [];
+    for (let i = 0; i < INVERSE_MATRIX.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < array.length; j++) {            
+            sum += array[j] * INVERSE_MATRIX[i][j];
+        }               
+        newArray.push(Math.round(sum));
+    }    
+    return newArray;
+}
+const decodeMatrix = encryptedMatrixMessage =>{        
+    const decryptedMatrixMessage = [];                 
+    for (let i = 0; i < encryptedMatrixMessage.length; i++) {                
+        decryptedMatrixMessage.push(decodeNumbers(encryptedMatrixMessage[i]));
+    }        
+    return decryptedMatrixMessage;    
+}
+const decodeMessageMatrix = decryptedMatrixMessage =>{
+    let initialMessage = '';
+    for (let i = 0; i < decryptedMatrixMessage.length; i++) {
+        for (let j = 0; j < decryptedMatrixMessage[i].length; j++) {
+            initialMessage += getLetter(decryptedMatrixMessage[i][j]);            
+        }
+    }
+    console.log(decryptedMatrixMessage)
+    return initialMessage;
+}
+const getLetter = number =>{
+    if(number == 28) return '.';
+    else if(number == 0) return ' ';
+    else return USA_ALPHABET[number - 1];
+}
 const getNumber = letter =>{    
     if(letter == " ") return 0;
     if(letter == ".") return 28;
@@ -45,7 +90,7 @@ const stringToStringNumbers = string =>{
     }    
     return stringNumbers;
 }
-const decodeNumbers = array =>{
+const encodeNumbers = array =>{
     const newArray = [];
     for (let i = 0; i < MATRIX.length; i++) {
         let sum = 0;
@@ -56,7 +101,7 @@ const decodeNumbers = array =>{
     }    
     return newArray;
 }
-const decodeMatrix = (encryptedRot13Message) =>{
+const encodeMatrix = (encryptedRot13Message) =>{
     
     const stringNumbers = stringToStringNumbers(encryptedRot13Message);
     const arrayNumbers = stringNumbers.split(",");    
@@ -67,13 +112,12 @@ const decodeMatrix = (encryptedRot13Message) =>{
         const newArray = arrayNumbers.slice(accountant, accountant + 3);
         accountant += 3;        
         if(newArray.length != 0){
-            encryptedMatrixMessage.push(decodeNumbers(newArray));         
+            encryptedMatrixMessage.push(encodeNumbers(newArray));         
         }        
     }    
     return encryptedMatrixMessage;
 }
-
-const decodeRot13 = ORIGINAL_MESSAGE =>{    
+const encodeRot13 = ORIGINAL_MESSAGE =>{    
     let decorderRotNewMessage = '';
     for (let i = 0; i < ORIGINAL_MESSAGE.length; i++) {        
         decorderRotNewMessage += newLetterRot(ORIGINAL_MESSAGE[i]);    
